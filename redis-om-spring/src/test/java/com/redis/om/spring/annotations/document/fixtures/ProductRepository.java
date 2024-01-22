c2df7d890ef66a115b05bf3bc544e0225045705a
Brian Sam-Bodden <bsb@redis.com>
2024-01-22 15:06:10 -0700

    Update ProductRepository.java
    fix: example ProductRepository incorrectly extending RedisEnhancedRepository instead of RedisDocumentRepository

--- redis-om-spring/src/test/java/com/redis/om/spring/annotations/document/fixtures/ProductRepository.java
+++ redis-om-spring/src/test/java/com/redis/om/spring/annotations/document/fixtures/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.redis.om.spring.annotations.document.fixtures;
 
-import com.redis.om.spring.repository.RedisEnhancedRepository;
+import com.redis.om.spring.repository.RedisDocumentRepository;
 
 import java.util.Optional;
 
-public interface ProductRepository extends RedisEnhancedRepository<Product, String> {
+public interface ProductRepository extends RedisDocumentRepository<Product, String> {
   Optional<Product> findFirstByName(String name);
 }
